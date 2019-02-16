f0c38fe9fe66c9342a51cfa8bb3c8c5f6bf3055a
shunw <shunw.li@outlook.com>
2019-02-16 21:54:28 +0800

    correct the column names in the db

---
+++ assets/data/sql2json.py
@@ -0,0 +1,36 @@
+import json
+import sqlite3
+
+class sql2json(object):
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.conn.row_factory = sqlite3.Row
+        self.db = self.conn.cursor()
+    
+    def sql_time(self):
+        ### get all the table name 
+        # res = self.conn.execute("select name from sqlite_master where type = 'table'")
+
+        ### get column name in the table
+        # pragma table_info(category_lkup);
+
+        self.rows = self.db.execute('''
+            select item.title, m.start, m.end, cat.category_name
+            from main_time m, item_lkup item, category_lkup cat
+            where item.item_id == m.item_id and item.category_id == cat.category_id
+        ''').fetchall()
+        
+        self.conn.commit()
+        self.conn.close()
+
+        return json.dumps( [dict(ix) for ix in self.rows] ) #CREATE JSON
+
+    def final_run(self):
+        
+        print (self.sql_time())
+
+
+if __name__ == '__main__':
+    trans = sql2json('./time_data.db')
+    trans.final_run()
