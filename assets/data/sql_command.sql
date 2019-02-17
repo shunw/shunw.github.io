d9998b4a7c90be9be21027cbff4a8e4661159135
shunw <shunw.li@outlook.com>
2019-02-17 10:24:23 +0800

    change sql command

--- assets/data/sql_command.sql
+++ assets/data/sql_command.sql
@@ -1,6 +1,5 @@
-update category_lkup set color='#ef908b' where category_name = 'machine_learning';
-update category_lkup set color='#eab798' where category_name = 'other_computer_science';
-update category_lkup set color='#f2baba' where category_name = 'python';
-update category_lkup set color='#9da1e0' where category_name = 'language';
-update category_lkup set color='#c2e8c8' where category_name = 'music';
-update category_lkup set color='#d6bbea' where category_name = 'hobby';
+-- ****************
+-- UPDATE COL INFO
+-- ****************
+
+-- update category_lkup set color='#ef908b' where category_name = 'machine_learning';
